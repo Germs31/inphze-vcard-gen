32dcababba331e143950faff8923aa85e09bd5bf
German Nunez <german90nunez@gmail.com>
2026-07-18 11:39:25 -0700

    added image, user can replace with their own

--- generate_qr.py
+++ generate_qr.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import qrcode
+from PIL import Image
 
 from contact_data import CONTACT
 from vcard import build_vcard
@@ -8,20 +9,36 @@ from vcard import build_vcard
 
 OUTPUT_DIR = Path("output")
 PNG_PATH = OUTPUT_DIR / "contact-qr.png"
+LOGO_PATH = Path("assets/logo.png")
+LOGO_SIZE_RATIO = .30
 
 
 def generate_png_qr(data: str, file_path: Path) -> None:
     qr = qrcode.QRCode(
         version=None,
-        error_correction=qrcode.constants.ERROR_CORRECT_M,
-        box_size=10,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=20,
         border=4,
     )
 
     qr.add_data(data)
     qr.make(fit=True)
 
-    image = qr.make_image(fill_color="black", back_color="white")
+    image = qr.make_image(fill_color="black", back_color="white").convert("RGBA")
+
+    if LOGO_PATH.exists():
+        logo = Image.open(LOGO_PATH).convert("RGBA")
+
+        logo_size = int(image.size[0] * LOGO_SIZE_RATIO)
+        logo.thumbnail((logo_size, logo_size))
+
+        logo_position = (
+            (image.size[0] - logo.size[0]) // 2,
+            (image.size[1] - logo.size[1]) // 2,
+        )
+
+        image.alpha_composite(logo, logo_position)
+
     image.save(file_path)
 
 
